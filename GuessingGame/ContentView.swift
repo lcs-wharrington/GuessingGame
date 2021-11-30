0805cf2a881e7f8de658ac36dfe77f4f8e491352
William Harrington <wharrington@lcs.on.ca>
2021-11-30 14:57:30 -0500

    Get the first model of the app up, presenting a navigation tile, a slider and a button.

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored Property
+    @State var currentGuess: Double = 50
+    
+    //the target the user is trying
+    let target = Int.random(in: 1...100)
+    
+    // MARK: Computed Property
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+
+            Text("\(String(format: "%.0f", currentGuess))")
+                .font(.title2)
+                .bold()
+            
+            
+            Slider(value: $currentGuess,
+                   in: 0.0...100.0,
+                   label: {
+                Text("Guess")
+            },
+                   minimumValueLabel: {
+                Text("0.0")
+            },
+                   maximumValueLabel: {
+                Text("100.0")
+            })
+            
+            Button (action:{
+                print("Guess Submited")
+            }, label: {
+                Text("Submit Guess")
+            })
+                .buttonStyle(.bordered)
+
+            Text("The actual secret number is: \(target).")
+            
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Guessing game")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
